226770d9560e8181a342a2195dd6ba2150c4db23
Abel Borit <abelboritguitton@gmail.com>
2024-02-01 17:05:38 -0500

    [FEAT] custom-label.directive using "ng g directive shared/directives/customLabel"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomLabelDirective } from './directives/custom-label.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CustomLabelDirective
+  ],
   imports: [
     CommonModule
   ]
