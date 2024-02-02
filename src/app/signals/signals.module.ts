412e963c37f03e59bcd3c1e0bd3133decd243333
Abel Borit <abelboritguitton@gmail.com>
2024-02-02 16:20:07 -0500

    [FEAT] user-info-page.component using "ng g component signals/pages/userInfoPage --skip-selector"

--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SignalsRoutingModule } from './signals-routing.module';
 import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
 import { CounterPageComponent } from './pages/counter-page/counter-page.component';
+import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
 
 
 @NgModule({
   declarations: [
     SignalsLayoutComponent,
-    CounterPageComponent
+    CounterPageComponent,
+    UserInfoPageComponent
   ],
   imports: [
     CommonModule,
