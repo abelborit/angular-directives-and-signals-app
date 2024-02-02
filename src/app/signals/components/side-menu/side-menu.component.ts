48046865d22924a1874651871cf5a1a94cd335cd
Abel Borit <abelboritguitton@gmail.com>
2024-02-02 17:00:35 -0500

    [FEAT] side-menu.component

--- src/app/signals/components/side-menu/side-menu.component.ts
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'signals-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.css']
+  styleUrls: ['./side-menu.component.css'],
 })
 export class SideMenuComponent {
-
+  public menuItems: MenuItem[] = [
+    { name: 'Contador', route: 'counter' },
+    { name: 'Usuario', route: 'user-info' },
+    { name: 'Mutaciones', route: 'properties' },
+  ];
 }
