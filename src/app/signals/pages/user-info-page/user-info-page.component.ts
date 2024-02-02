1b530ebfe55cc8df5926dc61976dcd43742db54f
Abel Borit <abelboritguitton@gmail.com>
2024-02-02 17:30:29 -0500

    [FEAT] add selector property to avoid error "Component ID generation collision detected."

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'signals-user-info-page',
   templateUrl: './user-info-page.component.html',
-  styleUrls: ['./user-info-page.component.css']
+  styleUrls: ['./user-info-page.component.css'],
 })
-export class UserInfoPageComponent {
-
-}
+export class UserInfoPageComponent {}
