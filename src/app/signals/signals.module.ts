10d4f9de41e29b9f17f2ff41bcae14f83438990a
Abel Borit <abelboritguitton@gmail.com>
2024-02-02 16:13:01 -0500

    [FEAT] counter-page.component using "ng g component signals/pages/counterPage --skip-selector"

--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SignalsRoutingModule } from './signals-routing.module';
 import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
+import { CounterPageComponent } from './pages/counter-page/counter-page.component';
 
 
 @NgModule({
   declarations: [
-    SignalsLayoutComponent
+    SignalsLayoutComponent,
+    CounterPageComponent
   ],
   imports: [
     CommonModule,
